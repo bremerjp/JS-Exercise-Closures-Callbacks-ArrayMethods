d6a07f970ba3fd4e55d352de1e720528bb7da5e4
Jacob Bremer <bremerjp@gmail.com>
2019-11-12 17:25:56 -0500

    Completed 2nd section

--- index.js
+++ index.js
@@ -180,8 +180,13 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+
+function getFullNames(runners) {
+  let runnerNames = [];
+  runners.forEach(function(runners) {
+    return runnerNames.push(`${runners.last_name}, ${runners.first_name}`);
+  });
+  return runnerNames;
 }
 
 /**
@@ -196,8 +201,16 @@ function getFullNames(/* CODE HERE */) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+
+// function firstNamesAllCaps(runners) {
+//   let uppercaseNames = runners.map(function(runners) {
+//     return `${runners.first_name}`.toUpperCase();
+//   })
+//   return uppercaseNames;
+// }
+
+function firstNamesAllCaps(runners) {
+  return runners.map(runners => `${runners.first_name}`.toUpperCase());
 }
 
 /**
@@ -213,8 +226,8 @@ function firstNamesAllCaps(/* CODE HERE */) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(runners, tShirtSize) {
+  return runners.filter(runner => runner.shirt_size === tShirtSize);
 }
 
 /**
@@ -227,8 +240,11 @@ function getRunnersByTShirtSize(/* CODE HERE */) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
-  /* CODE HERE */
+
+function tallyUpDonations(runners) {
+  return runners.reduce((sum, runner) => {
+    return sum + runner.donation;
+  }, 0);
 }
 
 /////////////// CLOSURES ///////////////
